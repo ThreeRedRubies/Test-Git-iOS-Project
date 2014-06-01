e673b4d1cdb5daedf4f91409477d6077b2dbfcf7
ThreeRedRubies <gouthrox@gmail.com>
2014-06-01 15:46:58 +1000

    Added a test class

---
+++ Git Test/TTTTest.h
@@ -0,0 +1,13 @@
+//
+//  TTTTest.h
+//  Git Test
+//
+//  Created by Goutham on 1/06/2014.
+//  Copyright (c) 2014 Three Red Rubies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TTTTest : NSObject
+
+@end
---
+++ Git Test/TTTTest.m
@@ -0,0 +1,13 @@
+//
+//  TTTTest.m
+//  Git Test
+//
+//  Created by Goutham on 1/06/2014.
+//  Copyright (c) 2014 Three Red Rubies. All rights reserved.
+//
+
+#import "TTTTest.h"
+
+@implementation TTTTest
+
+@end
